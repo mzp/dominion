060394a5e7e1eea0f42ae666f611f9446eb58e5a
mzp <mzpppp@gmail.com>
2010-08-11 11:59:56 +0900

    ルールの解釈結果をCPSにしてみた

--- card/definition.v
+++ card/definition.v
@@ -14,24 +14,38 @@ Inductive source :=
 | Put  (_ _: source)
 | And (_ _ : source).
 
+Definition card := nat.
+Definition bindings  := list (nat * nat).
+
 (* ユーザの入力 *)
+Definition cont A := list card -> bindings -> A.
 Inductive input :=
-  Select (_ : future) (_ : source).
+  SelectHand (_ : future)  (_ : cont input)
+| Nop.
 
-Fixpoint inputs (s : source) : list input :=
+Fixpoint inputs (s : source) (bs : bindings) (k : cont input) : input :=
   match s with
-  | Hand | Deck | Discard | Trash =>
-      nil
-  | Draw x Hand =>
-    (Select x Hand)::nil
-  | Draw _ _ =>
-    nil
-  | Put s1 s2  =>
-    inputs s1 ++ inputs s2
-  | And s1 s2 =>
-    inputs s1 ++ inputs s2
-end.
-
+    | Hand =>
+      k nil bs
+    | Deck =>
+      k nil bs
+    | Discard =>
+      k nil bs
+    | Trash =>
+      k nil bs
+    | Draw x Hand =>
+      SelectHand x (fun cs bs' => k cs (bs' ++ bs))
+    | Draw x s =>
+      inputs s bs (fun cs bs' => k (* draw *) cs (bs' ++ bs))
+    | Put x y =>
+      inputs x bs (fun from bs' =>
+        inputs y (bs' ++ bs) (fun to bs'' =>
+          k (* put *) nil (bs'' ++ bs' ++ bs)))
+    | And x y =>
+      inputs x bs (fun _ bs' =>
+        inputs y (bs' ++ bs) (fun _ bs'' =>
+          k nil (bs'' ++ bs' ++ bs)))
+  end.
 
 (* 記述例 *)
 Definition cellar :=
@@ -39,5 +53,6 @@ Definition cellar :=
     And (Put (Draw (Bind x) Hand) Discard)
         (Draw (Ref x) Deck).
 
-Eval compute in (inputs cellar).
+
+Eval compute in (inputs cellar nil (fun _ _ => Nop)).
 
