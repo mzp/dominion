6fdad4f6ef461558c8d0d7a1899f377532fb1a49
mzp <mzpppp@gmail.com>
2010-08-11 20:12:29 +0900

    全体的にrefineした

--- core/cards.ml
+++ core/cards.ml
@@ -1,34 +1,53 @@
 open Base
 
 type 'a var =
-    Bind of int
-  | Ref  of int
-  | Const of 'a
-  | Min of 'a var * 'a var
+    [ `Bind of int
+    | `Ref  of int
+    | `Const of 'a
+    | `Min of 'a var * 'a var ]
+
+type pred =
+    [ `Cost of int ]
 
 type source =
-    Hands
-  | Decks
-  | Discards
-  | Trash
+    [ `Hands
+    | `Decks
+    | `Discards
+    | `Trash
+    | `Supply
+    | `Filter of pred * source ]
 
 type action =
-  | Draw of int var * source
-  | Put  of action * source
-  | And  of action * action
+  [ `Draw of int var * source
+  | `Put  of action * source
+  | `And  of action * action ]
 
 type card =
-  | Card of int (* and more ... *)
+    {
+      id   : int;
+      cost : int
+    }
+
+let (>>=) x f =
+  match x with
+      Some y ->
+	f y
+    | None ->
+	None
 
 let rec resolve bs : 'a var -> 'a  option = function
-    Bind _ ->
+    `Bind _ ->
       None
-  | Ref b ->
+  | `Ref b ->
       option (List.assoc b) bs
-  | Const x ->
+  | `Const x ->
       Some x
-  | Min(x,y) ->
-      min (resolve bs x) (resolve bs y)
+  | `Min(x,y) ->
+      resolve bs x >>=
+	(fun x' ->
+	   resolve bs y >>=
+	     (fun y' ->
+		Some (min x' y')))
 
 type player = {
   hands : card list;
@@ -39,17 +58,9 @@ type player = {
 type game = {
   me     : player;
   others : player list;
-  trashs : card list
+  trashs : card list;
+  supply : card list
 }
-
-let cellar =
-  let x = 0
-  in
-    And (Put((Draw (Bind x,Hands)),Discards),
-        (Put((Draw((Ref x),Decks)),Hands)))
-
-let chapel =
-  Put (Draw (Min (Const 4,Bind 0),Hands),Trash)
 
 type bindisgs =
     (int * int) list
@@ -58,37 +69,55 @@ type 'a cont =
     bindisgs -> card list -> game -> 'a
 
 type user_action =
-    SelectFromHands of int var * game    * user_action cont
-  | DrawFrom        of source  * int var * player * user_action cont
-  | Result          of game
+    SelectFrom of [ `Hands | `Supply ] * int var * game    * user_action cont
+  | DrawFrom   of source  * int var * player * user_action cont
+  | Result     of game
 
-let map ~f src {me; trashs} =
+let fun_of_pred = function
+    `Cost n ->
+      fun c -> c.cost <= n
+
+let rec map ~f src ({me; trashs; supply} as game) =
   match src with
-      Decks ->
+      `Decks ->
 	f me.decks
-    | Hands ->
+    | `Hands ->
 	f me.hands
-    | Discards ->
+    | `Discards ->
 	f me.discards
-    | Trash ->
+    | `Trash ->
 	f trashs
+    | `Supply ->
+	f supply
+    | `Filter (pred, src) ->
+	map src game ~f:(fun cs -> f (List.filter (fun_of_pred pred) cs))
 
-let update ~f src ({ me; trashs } as game) =
+let rec update ~f src ({ me; trashs; supply } as game) =
     match src with
-      Decks ->
+      `Decks ->
 	{ game with me = { me with decks = f me.decks } }
-    | Hands ->
+    | `Hands ->
 	{ game with me = { me with hands = f me.hands } }
-    | Discards ->
+    | `Discards ->
 	{ game with me = { me with discards = f me.discards } }
-    | Trash ->
+    | `Trash ->
 	{ game with trashs = f trashs }
+    | `Supply ->
+	{ game with supply = f supply }
+    | `Filter (pred,src) ->
+	update src game ~f:begin fun cs ->
+	  let (xs, ys) =
+	    List.partition (fun_of_pred pred) cs in
+	    (f xs)@ys
+	end
 
 let rec eval bs action game k =
   match action with
-    | Draw (x, Hands) ->
-	SelectFromHands (x, game, k)
-    | Draw (x,src) ->
+    | `Draw (x, `Hands) ->
+	SelectFrom (`Hands, x, game, k)
+    | `Draw (x, `Supply) ->
+	SelectFrom (`Supply, x, game, k)
+    | `Draw (x,src) ->
 	begin match resolve bs x with
 	    Some n ->
 	      k bs
@@ -97,27 +126,13 @@ let rec eval bs action game k =
 	  | None ->
 	      DrawFrom (src, x, game.me, k)
 	end
-    | Put (action, src) ->
+    | `Put (action, src) ->
 	eval bs action game begin fun bs cs g ->
 	  k bs cs (update src g ~f:(fun x -> cs @ x))
 	end
-    | And (xs, ys) ->
+    | `And (xs, ys) ->
 	eval bs xs game begin fun bs' _ g ->
 	  eval bs' ys g begin fun bs'' _ g' ->
 	    k bs'' [] g'
 	  end
 	end
-
-(* example *)
-
-let SelectFromHands (bs,g,k) = eval [] chapel
-  {me = {
-    decks=[Card 0; Card 1; Card 2; Card 3];
-    hands=[Card 4; Card 5; Card 6];
-    discards=[]
-  };
-   others = [];
-   trashs = []}
-  (fun _ _ p -> Result p);;
-
-let _ = k [(0,2)] [Card 4; Card 6] {g with me = {g.me with hands = [Card 5]}};;
