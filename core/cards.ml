12242b1a6781a2022d380a75445b326fff12637c
mzp <mzpppp@gmail.com>
2010-08-14 14:42:38 +0900

    first gameのカード(攻撃以外)を実装した

--- core/cards.ml
+++ core/cards.ml
@@ -32,10 +32,22 @@ let update ~f place g =
 	me g ~f:fun p -> { p with hands = f p.hands }
     | `supply ->
 	{g with supply = f g.supply }
+    | `trash ->
+	{g with trash = f g.trash }
 
 let move src dest xs g =
   update dest ~f:(fun ys -> xs ++ ys) @@
     update src ~f:(fun ys -> ys -- xs) g
+
+let cost n xs =
+  List.filter (fun {cost} -> cost <= n) xs
+
+let one =
+  `Const 1
+let two =
+  `Const 2
+let any =
+  `Any
 
 let action n =
   me ~f:(fun ({action} as p) -> { p with action = action + n })
@@ -55,23 +67,56 @@ type 'a action =
 
 let cellar p g =
   perform begin
-    (xs : card list) <-- user p @@ selectFrom g g.me.hands `Any;
+    xs <-- user p @@ selectFrom g g.me.hands any;
     let g = move `hands `discards xs g in
-    (ys : card list) <-- user p @@ selectFrom g g.supply @@ `Const (List.length xs);
+    ys <-- user p @@ selectFrom g g.supply @@ `Const (List.length xs);
     ret @@ move `supply `hands ys g
   end
 
-let market p g =
-  ret @@ (g
-	  +> action 1
-	  +> buy    1
-	  +> coin   1
-	  +> draw   1)
+let market _ g =
+  g
+  +> action 1
+  +> buy    1
+  +> coin   1
+  +> draw   1
+  +> ret
 
-let mine _ = assert false
+let mine p g =
+  perform begin
+    (* todo: コインだけを捨てるようにする *)
+    [ x ] <-- user p @@ selectFrom g g.me.hands one;
+    let g = move `hands `trash [x] g in
+    ys <-- user p @@ selectFrom g (cost (x.cost + 3) g.supply) one;
+    ret @@ move `supply `hands ys g
+  end
 
-let remodel _ = assert false
-let smithy _ = assert false
-let village _ = assert false
-let woodcutter _ = assert false
-let workshop _ = assert false
+let remodel p g =
+  perform begin
+    [ x ] <-- user p @@ selectFrom g g.me.hands one;
+    let g = move `hands `trash [ x ] g in
+    ys <-- user p @@ selectFrom g (cost (x.cost + 2) g.supply) one;
+    ret @@ move `supply `discards ys g
+  end
+
+let smithy _ g =
+  g
+  +> draw 3
+  +> ret
+
+let village _ g =
+  g
+  +> action 2
+  +> draw 1
+  +> ret
+
+let woodcutter _ g =
+  g
+  +> buy 1
+  +> coin 2
+  +> ret
+
+let workshop p g =
+  perform begin
+    xs <-- user p @@ selectFrom g (cost 4 g.supply) one;
+    ret @@ move `supply `discards xs g
+  end
