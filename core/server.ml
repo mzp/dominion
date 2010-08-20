ced6fbc541b94a795be4652caa3a3ab12cd4dc38
mzp <mzpppp@gmail.com>
2010-08-20 09:08:57 +0900

    requestを部屋用に拡張した

--- core/server.ml
+++ core/server.ml
@@ -4,12 +4,20 @@ open Ccell
 type 'a ch = 'a Event.channel
 
 type response = [
+| `Ok
 | `Rooms of string list
+| `Chat  of string
+]
+
+type room_req = [
+| `Connect  of string
+| `Chat     of string * string
 ]
 
 type request = [
 | `ListRoom
 | `MakeRoom of string
+| room_req
 ]
 
 module type Transport = sig
@@ -20,34 +28,69 @@ end
 let daemon f =
   Thread.create (forever f) ()
 
-type state = {
-  rooms : string list
-}
-
-let state_daemon f init =
+let state_daemon ~f init =
   let rec g state =
     g (f state) in
     Thread.create g init
 
 module Make(T : Transport) = struct
+  type room = {
+    clients : response ch list;
+  }
+
+  type state = {
+    rooms : (string * (room_req * response ch) ch) list
+  }
+
   let empty = {
     rooms = []
   }
 
+  let make_room _ =
+    let ch =
+      Event.new_channel () in
+    let _ =
+      state_daemon {clients=[]} ~f:begin fun s ->
+	let (req, client) =
+	  Event.sync @@ Event.receive ch in
+	  match req with
+	    | `Connect _ ->
+		{ clients = client :: s.clients }
+	    | `Chat (_, msg) ->
+		if List.mem client s.clients then
+		  ();
+		s
+      end in
+      ch
+
+  let room_name = function
+    | `Connect name | `Chat (name,_) ->
+	name
+
   let master (ch : (request * response ch) ch) state =
     p "master" ();
-    match Event.sync @@ Event.receive ch with
-      | `ListRoom, client ->
-	  Event.sync @@ Event.send client (`Rooms state.rooms);
+    let (req, client) =
+      Event.sync @@ Event.receive ch in
+    match req with
+      | `ListRoom ->
+	  Event.sync @@ Event.send client (`Rooms (List.map fst state.rooms));
 	  state
-      | `MakeRoom s, _ ->
-	  { rooms = s :: state.rooms }
+      | `MakeRoom s ->
+	  { rooms = (s, make_room ()) :: state.rooms }
+      | #room_req as req ->
+	  begin match assoc (room_name req) state.rooms with
+	    | Some room ->
+		Event.sync @@ Event.send room (req, client)
+	    | None ->
+		()
+	  end;
+	  state
 
   let run host port =
     let ch =
       Event.new_channel () in
     let _ =
-      state_daemon (master ch) empty in
+      state_daemon ~f:(master ch) empty in
       T.server host port ~f:begin fun r w ->
 	p "accept" ();
 	ignore @@ Event.select [
@@ -63,11 +106,17 @@ module Make(T : Transport) = struct
       daemon begin fun () ->
 	match Event.sync @@ Event.receive r with
 	  | `Rooms [] ->
-	      p "--> no room" ()
+	      p "no room" ()
 	  | `Rooms xs ->
-	      List.iter (fun x -> p "--> %s" x ()) xs
+	      List.iter (fun x -> p "%s" x ()) xs
+	  | `Chat msg ->
+	      p "%s" msg ()
+	  | `Ok ->
+	      p "ok" ()
       end in
     let f _ = begin
+      print_string "$ ";
+      flush stdout;
       match read_line () with
 	  "ls" ->
 	    p "send ls" ();
