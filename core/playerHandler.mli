924f99e560e00e72adfd62199c4259efe9d43feb
mzp <mzpppp@gmail.com>
2010-08-27 07:19:34 +0900

    型シグネチャを変更

--- core/playerHandler.mli
+++ core/playerHandler.mli
@@ -4,24 +4,21 @@ module Make : functor(S : Protocol.Rpc) -> functor(B : HandlerBase.S with type t
   | `Skip
   ]
 
-  type state = B.state
-  val invoke : state -> state
-  val handle : S.t -> request -> state -> state
-
+  val invoke : B.state -> B.state
+  val handle : S.t -> request -> B.state -> B.state
 
   val action : ([> `Cc of
-		   state * ([> `Select of 'b2 | `Skip ] -> bool) *
-          ([< `Select of Game.card | `Skip ] -> state -> (unit, 'a2) Cc.CONT.mc) ] as 'a2)  Cc.prompt -> S.t -> state -> (unit, state) Cc.CONT.mc
+		   B.state * ([> `Select of 'b2 | `Skip ] -> bool) *
+          ([< `Select of Game.card | `Skip ] -> B.state -> (unit, 'a2) Cc.CONT.mc) ] as 'a2)  Cc.prompt -> S.t -> B.state -> (unit, B.state) Cc.CONT.mc
 
   val buy : ([> `Cc of
-		   state * ([> `Select of 'b2 | `Skip ] -> bool) *
-          ([< `Select of Game.card | `Skip ] -> state -> (unit, 'a2) Cc.CONT.mc) ] as 'a2)  Cc.prompt -> S.t -> state -> (unit, state) Cc.CONT.mc
+		   B.state * ([> `Select of 'b2 | `Skip ] -> bool) *
+          ([< `Select of Game.card | `Skip ] -> B.state -> (unit, 'a2) Cc.CONT.mc) ] as 'a2)  Cc.prompt -> S.t -> B.state -> (unit, B.state) Cc.CONT.mc
 
   val cleanup : ([> `Cc of
-		   state * ([> `Select of 'b2 | `Skip ] -> bool) *
-          ([< `Select of Game.card | `Skip ] -> state -> (unit, 'a2) Cc.CONT.mc) ] as 'a2)  Cc.prompt -> S.t -> state -> (unit, state) Cc.CONT.mc
+		   B.state * ([> `Select of 'b2 | `Skip ] -> bool) *
+          ([< `Select of Game.card | `Skip ] -> B.state -> (unit, 'a2) Cc.CONT.mc) ] as 'a2)  Cc.prompt -> S.t -> B.state -> (unit, B.state) Cc.CONT.mc
 
-
-  val game : state -> Game.t
-  val make_dummy : S.t list -> Game.t -> state
+  val game : B.state -> Game.t
+  val make_dummy : S.t list -> Game.t -> B.state
 end
