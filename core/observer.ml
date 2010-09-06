d88c3ca90480af2ac81c256f4757f1c08438b67e
mzp <mzpppp@gmail.com>
2010-09-06 20:07:17 +0900

    [実装]F#の第一級イベントを移植した

--- core/observer.ml
+++ core/observer.ml
@@ -1,12 +1,39 @@
 open Base
-type 'a t = int
 type 'a listener = 'a -> unit
+type 'a t = 'a listener list ref
 
 
-let listen _ = assert false
-let clear  _ = assert false
-let map    _ = assert false
-let filter _ = assert false
-let merge  _ = assert false
-let __fire _ = assert false
-let make   _ = assert false
+let listen o f =
+  o := f :: !o
+
+let make () =
+  ref []
+
+let __fire o x =
+  List.iter (fun f -> f x) !o
+
+let clear o =
+  o := []
+
+let map f o =
+  let o' =
+    make () in
+    listen o (fun x -> __fire o' @@ f x);
+    o'
+
+let filter f o =
+  let o' =
+    make () in
+    listen o (fun x ->
+		match f x with
+		    None -> ()
+		  | Some y -> __fire o' y);
+    o'
+
+let merge o1 o2 =
+  let o' =
+    make () in
+    listen o1 (fun x -> __fire o' (Left x));
+    listen o2 (fun x -> __fire o' (Right x));
+    o'
+
