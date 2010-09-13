13935f04591f8470008f863aa96395fa733efe1f
mzp <mzpppp@gmail.com>
2010-09-14 06:45:37 +0900

    [msgpack]Inductiveで変換ルールを記述するようにした

--- msgpack/Definition.v
+++ msgpack/Definition.v
@@ -1,26 +1,32 @@
 Require Import Strings.Ascii.
+Require Import List.
+
+Open Local Scope char_scope.
+Open Local Scope list_scope.
+
 Inductive data :=
-| nil.
+| Bool (_ : bool)
+| Nil.
 
-Definition serialize (x : data) : ascii :=
-  match x with
-    | nil =>
-      ascii_of_nat 192
-  end.
+Definition singleton {A : Type} (x : A) := x :: nil.
+Inductive Serialized : data -> list ascii -> Prop :=
+| SNil  :
+  Serialized Nil (singleton "192")
+| STrue :
+  Serialized (Bool true) (singleton "195")
+| SFalse :
+  Serialized (Bool false) (singleton "194").
 
-Definition deserialize (x : ascii) : option data :=
-  match nat_of_ascii x with
-    | 192 => Some nil
-    | _ => None
-  end.
-
-Theorem preserve : forall x,
-  deserialize (serialize x) = Some x.
+Theorem injective : forall x1 x2 y1 y2,
+  x1 <> y1 ->
+  Serialized x1 x2 ->
+  Serialized y1 y2 ->
+  x2 <> y2.
 Proof.
-induction x; intros; simpl.
- unfold deserialize.
- simpl.
- reflexivity.
+intros.
+inversion H0; inversion H1;
+  try (intro; discriminate);
+  try (rewrite <- H2, <- H4 in H;
+       assert False;
+         [ apply H; reflexivity | contradiction ]).
 Qed.
-
-Extraction "msgpack.ml" serialize deserialize.
