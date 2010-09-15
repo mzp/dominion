bbf4a29627d6041211bc62470b7f322b8ebe7061
mzp <mzpppp@gmail.com>
2010-09-15 21:42:49 +0900

    補題をつかってコードを減らした refs: 014

--- msgpack/Prefix.v
+++ msgpack/Prefix.v
@@ -25,6 +25,71 @@ split; intros.
   reflexivity.
 Qed.
 
+Lemma prefix8 : forall A (x1 y1 : A) tl1 tl2,
+  Prefix (x1::tl1) (y1::tl2) ->
+  x1 = y1.
+Proof.
+intros.
+apply prefix_inv in H.
+tauto.
+Qed.
+
+Lemma prefix16 : forall A (x1 x2 y1 y2 : A) tl1 tl2,
+  Prefix (x1::x2::tl1) (y1::y2::tl2) ->
+  x1 = y1 /\ x2 = y2.
+Proof.
+intros.
+apply prefix_inv in H.
+decompose [and] H.
+apply prefix_inv in H1.
+decompose [and] H1.
+tauto.
+Qed.
+
+Lemma prefix32 : forall A (x1 x2 x3 x4 y1 y2 y3 y4: A) tl1 tl2,
+  Prefix (x1::x2::x3::x4::tl1) (y1::y2::y3::y4::tl2) ->
+  x1 = y1 /\ x2 = y2 /\ x3 = y3 /\ x4 = y4.
+Proof.
+intros.
+apply prefix_inv in H.
+decompose [and] H.
+apply prefix_inv in H1.
+decompose [and] H1.
+apply prefix_inv in H3.
+decompose [and] H3.
+apply prefix_inv in H5.
+decompose [and] H5.
+tauto.
+Qed.
+
+
+Lemma prefix64 : forall A (x1 x2 x3 x4 x5 x6 x7 x8
+                           y1 y2 y3 y4 y5 y6 y7 y8 : A) tl1 tl2,
+  Prefix (x1::x2::x3::x4::x5::x6::x7::x8::tl1)
+         (y1::y2::y3::y4::y5::y6::y7::y8::tl2) ->
+         x1 = y1 /\ x2 = y2 /\ x3 = y3 /\ x4 = y4 /\
+         x5 = y5 /\ x6 = y6 /\ x7 = y7 /\ x8 = y8.
+Proof.
+intros.
+apply prefix_inv in H.
+decompose [and] H.
+apply prefix_inv in H1.
+decompose [and] H1.
+apply prefix_inv in H3.
+decompose [and] H3.
+apply prefix_inv in H5.
+decompose [and] H5.
+apply prefix_inv in H7.
+decompose [and] H7.
+apply prefix_inv in H9.
+decompose [and] H9.
+apply prefix_inv in H11.
+decompose [and] H11.
+apply prefix_inv in H13.
+decompose [and] H13.
+tauto.
+Qed.
+
 (* 行頭符号になってないことの証明 *)
 Theorem NotPrefixEncoding : forall x1 x2 y1 y2,
   ~(x1 =~ x2) ->
@@ -49,118 +114,53 @@ inversion H0; inversion H1;
   decompose [and] H2;
   (discriminate || auto).
 
- apply prefix_inv in H7.
+ apply prefix8 in H7.
  decompose [and] H7.
- rewrite H8.
+ rewrite H7.
  apply_data_eq.
 
- apply prefix_inv in H7.
+ apply prefix16 in H7.
  decompose [and] H7.
- apply prefix_inv in H9.
- decompose [and] H9.
- rewrite H8, H10.
+ rewrite H8, H9.
  apply_data_eq.
 
- apply prefix_inv in H7.
+ apply prefix32 in H7.
  decompose [and] H7.
- apply prefix_inv in H9.
- decompose [and] H9.
- apply prefix_inv in H11.
- decompose [and] H11.
- apply prefix_inv in H13.
- decompose [and] H13.
- rewrite H8,H10,H12,H14.
+ rewrite H8,H9,H10,H12.
  apply_data_eq.
 
- apply prefix_inv in H7.
+ apply prefix64 in H7.
  decompose [and] H7.
- apply prefix_inv in H9.
- decompose [and] H9.
- apply prefix_inv in H11.
- decompose [and] H11.
- apply prefix_inv in H13.
- decompose [and] H13.
- apply prefix_inv in H15.
- decompose [and] H15.
- apply prefix_inv in H17.
- decompose [and] H17.
- apply prefix_inv in H19.
- decompose [and] H19.
- apply prefix_inv in H21.
- decompose [and] H21.
- rewrite H8,H10,H12,H14,H16,H18,H20,H22.
+ rewrite H8,H9,H10,H11,H12,H13,H14,H16.
  apply_data_eq.
 
- apply prefix_inv in H7.
+ apply prefix8 in H7.
  decompose [and] H7.
- rewrite H8.
+ rewrite H7.
  apply_data_eq.
 
- apply prefix_inv in H7.
+ apply prefix16 in H7.
  decompose [and] H7.
- apply prefix_inv in H9.
- decompose [and] H9.
- rewrite H8, H10.
+ rewrite H8, H9.
  apply_data_eq.
 
- apply prefix_inv in H7.
+ apply prefix32 in H7.
  decompose [and] H7.
- apply prefix_inv in H9.
- decompose [and] H9.
- apply prefix_inv in H11.
- decompose [and] H11.
- apply prefix_inv in H13.
- decompose [and] H13.
- rewrite H8,H10,H12,H14.
+ rewrite H8,H9,H10,H12.
  apply_data_eq.
 
- apply prefix_inv in H7.
+ apply prefix64 in H7.
  decompose [and] H7.
- apply prefix_inv in H9.
- decompose [and] H9.
- apply prefix_inv in H11.
- decompose [and] H11.
- apply prefix_inv in H13.
- decompose [and] H13.
- apply prefix_inv in H15.
- decompose [and] H15.
- apply prefix_inv in H17.
- decompose [and] H17.
- apply prefix_inv in H19.
- decompose [and] H19.
- apply prefix_inv in H21.
- decompose [and] H21.
- rewrite H8,H10,H12,H14,H16,H18,H20,H22.
+ rewrite H8,H9,H10,H11,H12,H13,H14,H16.
  apply_data_eq.
 
- apply prefix_inv in H7.
+ apply prefix32 in H7.
  decompose [and] H7.
- apply prefix_inv in H9.
- decompose [and] H9.
- apply prefix_inv in H11.
- decompose [and] H11.
- apply prefix_inv in H13.
- decompose [and] H13.
- rewrite H8,H10,H12,H14.
+ rewrite H8,H9,H10,H12.
  apply_data_eq.
 
- apply prefix_inv in H7.
+ apply prefix64 in H7.
  decompose [and] H7.
- apply prefix_inv in H9.
- decompose [and] H9.
- apply prefix_inv in H11.
- decompose [and] H11.
- apply prefix_inv in H13.
- decompose [and] H13.
- apply prefix_inv in H15.
- decompose [and] H15.
- apply prefix_inv in H17.
- decompose [and] H17.
- apply prefix_inv in H19.
- decompose [and] H19.
- apply prefix_inv in H21.
- decompose [and] H21.
- rewrite H8,H10,H12,H14,H16,H18,H20,H22.
+ rewrite H8,H9,H10,H11,H12,H13,H14,H16.
  apply_data_eq.
 Qed.
-
