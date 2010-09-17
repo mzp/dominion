6f4a002132f05ad2678a456c9df080a038d06d33
mzp <mzpppp@gmail.com>
2010-09-17 11:20:04 +0900

    multi asciiに併せて、等値性の定義を変更した refs: 018

--- msgpack/MultiAscii.v
+++ msgpack/MultiAscii.v
@@ -27,8 +27,32 @@ Fixpoint drop (n :nat) (p : positive) :=
       end
   end.
 
+Fixpoint pow (m n : nat) :=
+  match n with
+    | 0 =>
+      1
+    | (S n') =>
+      m * pow m n'
+  end.
+
+Definition nat_of_ascii8 :=
+  nat_of_ascii.
+
 Definition ascii8_of_nat :=
   ascii_of_nat.
+
+Lemma soundness_ascii8 : forall c,
+  ascii8_of_nat (nat_of_ascii8 c) = c.
+Proof.
+apply ascii_nat_embedding.
+Qed.
+
+Definition nat_of_ascii16 bytes :=
+  match bytes with
+    (c1, c2) =>
+    nat_of_ascii c1 * pow 2 8
+    + nat_of_ascii c2
+  end.
 
 Definition ascii16_of_nat n :=
   match n with
@@ -37,6 +61,15 @@ Definition ascii16_of_nat n :=
       let p := P_of_succ_nat n in
         (to_ascii (drop 8 p),
           ascii_of_pos p)
+  end.
+
+Definition nat_of_ascii32 bytes :=
+  match bytes with
+    (c1, c2, c3, c4) =>
+    nat_of_ascii c1   * pow 2 24
+    + nat_of_ascii c2 * pow 2 16
+    + nat_of_ascii c3 * pow 2 8
+    + nat_of_ascii c4
   end.
 
 Definition ascii32_of_nat n :=
@@ -48,6 +81,19 @@ Definition ascii32_of_nat n :=
           to_ascii (drop 16 p),
           to_ascii (drop 8 p),
           ascii_of_pos p)
+  end.
+
+Definition nat_of_ascii64 bytes :=
+  match bytes with
+    (c1, c2, c3, c4, c5, c6, c7, c8) =>
+    nat_of_ascii   c1 * pow 2 56
+    + nat_of_ascii c2 * pow 2 48
+    + nat_of_ascii c3 * pow 2 40
+    + nat_of_ascii c4 * pow 2 32
+    + nat_of_ascii c5 * pow 2 24
+    + nat_of_ascii c6 * pow 2 16
+    + nat_of_ascii c7 * pow 2 8
+    + nat_of_ascii c8
   end.
 
 Definition ascii64_of_nat n :=
--- msgpack/Object.v
+++ msgpack/Object.v
@@ -1,38 +1,30 @@
-Require Import Ascii.
 Require Import List.
-
-Fixpoint pow (m n : nat) :=
-  match n with
-    | 0 =>
-      1
-    | (S n') =>
-      m * pow m n'
-  end.
+Require Import MultiAscii.
 
 Inductive object :=
 | Bool (_ : bool)
 | Nil
 | PFixnum: forall n,
-  nat_of_ascii n < 128 -> object
+  nat_of_ascii8 n < 128 -> object
 | NFixnum: forall n,
   (* 負の数を導入したくないので、補数表現を使う。 *)
-  223 < nat_of_ascii n /\ nat_of_ascii n < 256 ->
+  223 < nat_of_ascii8 n /\ nat_of_ascii8 n < 256 ->
   object
-| Uint8  (_ : ascii)
-| Uint16 (_ _ : ascii)
-| Uint32 (_ _ _ _ : ascii)
-| Uint64 (_ _ _ _ _ _ _ _ : ascii)
-| Int8   (_ : ascii)
-| Int16  (_ _ : ascii)
-| Int32  (_ _ _ _ : ascii)
-| Int64  (_ _ _ _ _ _ _ _ : ascii)
-| Float  (_ _ _ _ : ascii)
-| Double (_ _ _ _ _ _ _ _ : ascii)
-| FixRaw: forall (xs : list ascii),
+| Uint8  (_ : ascii8)
+| Uint16 (_ : ascii16)
+| Uint32 (_ : ascii32)
+| Uint64 (_ : ascii64)
+| Int8   (_ : ascii8)
+| Int16  (_ : ascii16)
+| Int32  (_ : ascii32)
+| Int64  (_ : ascii64)
+| Float  (_ : ascii32)
+| Double (_ : ascii64)
+| FixRaw: forall (xs : list ascii8),
   List.length xs < 32 -> object
-| Raw16: forall (xs : list ascii),
+| Raw16: forall (xs : list ascii8),
   List.length xs < pow 2 16 -> object
-| Raw32: forall (xs : list ascii),
+| Raw32: forall (xs : list ascii8),
   List.length xs < pow 2 32 -> object.
 
 (* データの等値性を定義。依存型の証明部分は無視する。*)
@@ -49,29 +41,32 @@ Inductive eq_object : object -> object -> Prop :=
   eq_object (NFixnum n P) (NFixnum m Q)
 | Uint8Eq : forall c,
   eq_object (Uint8 c) (Uint8 c)
-| Uint16Eq : forall c1 c2,
-  eq_object (Uint16 c1 c2) (Uint16 c1 c2)
-| Uint32Eq : forall c1 c2 c3 c4,
-  eq_object (Uint32 c1 c2 c3 c4) (Uint32 c1 c2 c3 c4)
-| Uint64Eq : forall c1 c2 c3 c4 c5 c6 c7 c8,
-  eq_object (Uint64 c1 c2 c3 c4 c5 c6 c7 c8) (Uint64 c1 c2 c3 c4 c5 c6 c7 c8)
+| Uint16Eq : forall c,
+  eq_object (Uint16 c) (Uint16 c)
+| Uint32Eq : forall c,
+  eq_object (Uint32 c) (Uint32 c)
+| Uint64Eq : forall c,
+  eq_object (Uint64 c) (Uint64 c)
 | Int8Eq : forall c,
   eq_object (Int8 c) (Int8 c)
-| Int16Eq : forall c1 c2,
-  eq_object (Int16 c1 c2) (Int16 c1 c2)
-| Int32Eq : forall c1 c2 c3 c4,
-  eq_object (Int32 c1 c2 c3 c4) (Int32 c1 c2 c3 c4)
-| Int64Eq : forall c1 c2 c3 c4 c5 c6 c7 c8,
-  eq_object (Int64 c1 c2 c3 c4 c5 c6 c7 c8) (Int64 c1 c2 c3 c4 c5 c6 c7 c8)
-| FloatEq : forall c1 c2 c3 c4,
-  eq_object (Float c1 c2 c3 c4) (Float c1 c2 c3 c4)
-| DoubleEq : forall c1 c2 c3 c4 c5 c6 c7 c8,
-  eq_object (Double c1 c2 c3 c4 c5 c6 c7 c8) (Double c1 c2 c3 c4 c5 c6 c7 c8)
+| Int16Eq : forall c,
+  eq_object (Int16 c) (Int16 c)
+| Int32Eq : forall c,
+  eq_object (Int32 c) (Int32 c)
+| Int64Eq : forall c,
+  eq_object (Int64 c) (Int64 c)
+| FloatEq : forall c,
+  eq_object (Float c) (Float c)
+| DoubleEq : forall c,
+  eq_object (Double c) (Double c)
 | FixRawEq : forall xs ys P Q,
+  xs = ys ->
   eq_object (FixRaw xs P) (FixRaw ys Q)
 | Raw16Eq : forall xs ys P Q,
+  xs = ys ->
   eq_object (Raw16 xs P) (Raw16 ys Q)
 | Raw32Eq : forall xs ys P Q,
+  xs = ys ->
   eq_object (Raw32 xs P) (Raw32 ys Q).
 
 Infix "=~" := eq_object (at level 60, right associativity).
@@ -100,22 +95,14 @@ Ltac apply_object_eq :=
 Lemma eq_sym : forall x,
   x =~ x.
 Proof.
-destruct x; apply_object_eq.
- reflexivity.
-
- reflexivity.
+destruct x; apply_object_eq; auto.
 Qed.
 
 Lemma eq_refl: forall x y,
   x =~ y -> y =~ x.
 Proof.
 intros.
-inversion H; apply_object_eq.
- rewrite H0.
- reflexivity.
-
- rewrite H0.
- reflexivity.
+inversion H; apply_object_eq; auto.
 Qed.
 
 Lemma eq_trans: forall x y z,
@@ -125,13 +112,8 @@ intros.
 inversion H;
   rewrite <- H2 in *; inversion H0;
     apply_object_eq || (rewrite <- H3 in *; inversion H4);
-    try inversion H5.
-
- rewrite H1, <- H3, <- H4 in *.
- inversion H5.
- reflexivity.
-
- rewrite H1, <- H3, <- H4 in *.
- inversion H5.
- reflexivity.
+    try inversion H5;
+    try (rewrite H1, <- H3, <- H4 in *;
+         inversion H5;
+         reflexivity).
 Qed.
