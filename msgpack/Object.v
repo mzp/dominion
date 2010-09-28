b94a33c882123cb589e53416cee642a9cdd80b39
mzp <mzpppp@gmail.com>
2010-09-29 06:55:40 +0900

    objectのindの改良版を定義した

--- msgpack/Object.v
+++ msgpack/Object.v
@@ -53,3 +53,89 @@ Inductive Valid : object -> Prop :=
   length xs < pow 16 -> Valid (Map16 xs)
 | VMap32    : forall xs,
   length xs < pow 32 -> Valid (Map32 xs).
+
+Lemma object_ind0 : forall P : object -> Prop,
+  (forall b : bool, P (Bool b)) ->
+  P Nil ->
+  (forall a : ascii8, P (PFixnum a)) ->
+  (forall a : ascii8, P (NFixnum a)) ->
+  (forall a : ascii8, P (Uint8 a)) ->
+  (forall a : ascii16, P (Uint16 a)) ->
+  (forall a : ascii32, P (Uint32 a)) ->
+  (forall a : ascii64, P (Uint64 a)) ->
+  (forall a : ascii8, P (Int8 a)) ->
+  (forall a : ascii16, P (Int16 a)) ->
+  (forall a : ascii32, P (Int32 a)) ->
+  (forall a : ascii64, P (Int64 a)) ->
+  (forall a : ascii32, P (Float a)) ->
+  (forall a : ascii64, P (Double a)) ->
+  (forall l : list ascii8, P (FixRaw l)) ->
+  (forall l : list ascii8, P (Raw16 l)) ->
+  (forall l : list ascii8, P (Raw32 l)) ->
+  P (FixArray nil) ->
+  (forall (x : object) (xs : list object),
+    P x -> P (FixArray xs) -> P (FixArray (x :: xs))) ->
+  P (Array16 nil) ->
+  (forall (x : object) (xs : list object),
+    P x -> P (Array16 xs) -> P (Array16 (x :: xs))) ->
+  P (Array32 nil) ->
+  (forall (x : object) (xs : list object),
+    P x -> P (Array32 xs) -> P (Array32 (x :: xs))) ->
+  P (FixMap nil) ->
+  (forall (x y : object) (xs : list (object * object)),
+    P x -> P y -> P (FixMap xs) -> P (FixMap ((x, y) :: xs))) ->
+  P (Map16 nil) ->
+  (forall (x y : object) (xs : list (object * object)),
+    P x -> P y -> P (Map16 xs) -> P (Map16 ((x, y) :: xs))) ->
+  P (Map32 nil) ->
+  (forall (x y : object) (xs : list (object * object)),
+    P x -> P y -> P (Map32 xs) -> P (Map32 ((x, y) :: xs))) ->
+  (forall o, P o).
+Proof.
+intros until o.
+generalize o.
+Check H23.
+refine (fix obj_resolve o : P o :=
+  match o with
+    | FixArray xs =>
+      (fix array_resolve (xs' : list object) : P (FixArray xs') :=
+        match xs' with
+          | nil => H16
+          | x::xs => H17 x xs (obj_resolve x) _
+        end) xs
+    | Array16  xs =>
+      (fix array_resolve (xs' : list object) : P (Array16 xs') :=
+        match xs' with
+          | nil =>H18
+          | x::xs => H19 x xs (obj_resolve x) _
+        end) xs
+    | Array32  xs =>
+      (fix array_resolve (xs' : list object) : P (Array32 xs') :=
+        match xs' with
+          | nil => H20
+          | x::xs => H21 x xs (obj_resolve x) _
+        end) xs
+    | FixMap   xs =>
+      (fix map_resolve (xs' : list (object * object)%type) : P (FixMap xs') :=
+        match xs' with
+          | nil => H22
+          | (x,y)::xs =>
+            H23 x y xs (obj_resolve x) (obj_resolve y) _
+        end) xs
+    | Map16    xs =>
+      (fix map_resolve (xs' : list (object * object)%type) : P (Map16 xs') :=
+        match xs' with
+          | nil => H24
+          | (x,y)::xs =>
+            H25 x y xs (obj_resolve x) (obj_resolve y) _
+        end) xs
+    | Map32    xs =>
+      (fix map_resolve (xs' : list (object * object)%type) : P (Map32 xs') :=
+        match xs' with
+          | nil => H26
+          | (x,y)::xs =>
+            H27 x y xs (obj_resolve x) (obj_resolve y) _
+        end) xs
+    | _ => _
+  end); auto.
+Qed.
